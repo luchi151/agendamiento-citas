96b18f4a80b05e685da295867a0c089d596d3e34
luchi151 <lmcferrira@gmail.com>
2025-12-10 14:42:32 -0500

    update_middleware

--- agendamientoCitas/config/settings.py
+++ agendamientoCitas/config/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'usuarios.middleware.AsesorRedirectMiddleware',
+    'usuarios.middleware.AsesorRedirectMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
